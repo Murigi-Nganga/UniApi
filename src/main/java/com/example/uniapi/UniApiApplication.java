334d3743230f9294ad5d39f379e929727c91d53d
Murigi-Nganga <murigigracious@students.uonbi.ac.ke>
2024-02-24 11:08:02 +0300

    initial commit

---
+++ src/main/java/com/example/uniapi/UniApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uniapi/UniApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
